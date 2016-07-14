4b4bf89e5116f9c586c088400784035b2925d229
Ash <fangelashf@gmail.com>
2016-07-14 22:03:26 +0800

    动态场景切割

---
+++ Assets/Editor/SceneSlicer.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class SceneSlicer
+{
+    [MenuItem("SceneSlicer/SliceScene")]
+    public static void SliceScene()
+    {
+        var wizard = EditorWindow.GetWindow<SceneSlicerWizard>();
+        wizard.Show();
+    }
+}
+
+public class SceneSlicerWizard : EditorWindow
+{
+    private Vector2 scrollPos;
+    private GameObject m_scenePrefab;
+    private int selected;
+
+    private void OnGUI()
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        m_scenePrefab = EditorGUILayout.ObjectField("scene prefab", m_scenePrefab, typeof(GameObject), false) as GameObject;
+        selected = GUILayout.SelectionGrid(selected, new string[] { "2x2", "4x4", "8x8", "16x16", "32x32", "64x64" }, 6);
+        if (GUILayout.Button("begin slice"))
+        {
+            if (m_scenePrefab == null)
+            {
+                EditorUtility.DisplayDialog("Error", "scene prefab empty.", "OK");
+            }
+            else
+            {
+                //var node = GameObject.Instantiate(m_scenePrefab.transform.FindChild("Cacti"));
+                SliceScene(m_scenePrefab, (int)Math.Pow(2, selected + 1));
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void SliceScene(GameObject scenePrefab, int dimension)
+    {
+        Debug.Log("dimension " + dimension);
+        Terrain sceneTerrain = scenePrefab.GetComponentInChildren<Terrain>();
+
+        if (sceneTerrain != null)
+        {
+            var startPoint = sceneTerrain.transform.position;
+            var sceneSize = sceneTerrain.terrainData.size;
+
+            var cellX = sceneSize.x / dimension;
+            var cellZ = sceneSize.z / dimension;
+
+            var cellPos = new Vector2[dimension, dimension];
+            var cellEndPos = new Vector2[dimension, dimension];
+            var cellPrefab = new GameObject[dimension, dimension];
+
+            var rubbishNode = new GameObject();//用来存放复制节点时多出来的子节点，然后一起删掉
+            rubbishNode.name = "rubbish";
+            rubbishNode.SetActive(false);
+
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    cellPos[i, j] = new Vector2(startPoint.x + j * cellX, startPoint.z + i * cellZ);
+                    cellEndPos[i, j] = new Vector2(startPoint.x + (j + 1) * cellX, startPoint.z + (i + 1) * cellZ);
+                    var go = new GameObject();
+                    go.name = string.Format("{0}_{1}_{2}", scenePrefab.name, i + 1, j + 1);
+                    cellPrefab[i, j] = go;
+                }
+            }
+
+            var children = GetAllChildren(scenePrefab.transform);
+            foreach (var child in children)
+            {
+                if (child.gameObject == sceneTerrain.gameObject)
+                    continue;
+                var isConstructorNode = false;//是否为结构节点
+                if(child.transform.childCount != 0)
+                {
+                    isConstructorNode = true;
+                    if (child.gameObject.GetComponent<MeshRenderer>() != null
+                    || child.gameObject.GetComponent<Light>() != null
+                    || child.gameObject.GetComponent<ParticleSystem>() != null)
+                    {
+                        Debug.LogError("child contain component: " + child);//违反设定，结构节点不能作为显示元素
+                    }
+                }
+                var hasHandle = false;
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (hasHandle)
+                        break;
+                    for (int j = 0; j < dimension; j++)
+                    {
+                        if (isConstructorNode)
+                        {
+                            var node = GameObject.Instantiate(child);
+                            CleanUpNewTran(node, rubbishNode.transform);
+                            node.name = child.name;
+                            node.transform.parent = FindCellPrefabParentByTargetTran(child, cellPrefab[i, j].transform);
+                        }
+                        else
+                        {
+                            var x = child.position.x;
+                            var z = child.position.z;
+                            if (cellPos[i, j].x < x && x < cellEndPos[i, j].x && cellPos[i, j].y < z && z < cellEndPos[i, j].y)
+                            {
+                                var node = GameObject.Instantiate(child);
+                                CleanUpNewTran(node, rubbishNode.transform);
+                                node.name = child.name;
+                                node.transform.parent = FindCellPrefabParentByTargetTran(child, cellPrefab[i, j].transform);
+                                hasHandle = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            Debug.Log("children " + children.Count);
+
+            Debug.Log(sceneTerrain + " " + startPoint + " " + sceneSize);
+
+            GameObject.DestroyImmediate(rubbishNode);
+        }
+    }
+
+    private void CleanUpNewTran(Transform node, Transform rubbishNode)
+    {
+        List<Transform> temp = new List<Transform>();
+        foreach (Transform item in node)
+        {
+            temp.Add(item);
+        }
+        foreach (var item in temp)
+        {
+            item.parent = rubbishNode.transform;
+        }
+    }
+
+    private Transform FindCellPrefabParentByTargetTran(Transform sourceTran, Transform cellTran)
+    {
+        var stack = new Stack<string>();
+        var sourceParent = sourceTran;
+        while (sourceParent.parent != null)
+        {
+            stack.Push(sourceParent.parent.name);
+            sourceParent = sourceParent.parent;
+        }
+
+        Transform result = cellTran;
+        while (stack.Count != 0)
+        {
+            var child = cellTran.FindChild(stack.Pop());
+            if (child != null)
+            {
+                result = child;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return result;
+    }
+
+    private List<Transform> GetAllChildren(Transform tran)
+    {
+        List<Transform> children = new List<Transform>();
+        foreach (Transform item in tran)
+        {
+            children.Add(item);
+            children.AddRange(GetAllChildren(item));
+        }
+        return children;
+    }
+}
---
+++ Assets/Scripts/DynamicScenes.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicScenes : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Canvas MapSurface;
+    int mapSectionWidth, mapSectionHeight, mapSectionXNum, mapSectionYNum;
+
+    int _leaderSectionX;
+    /// <summary>
+    /// 主角所处的切片X值
+    /// </summary>
+    public int leaderSectionX
+    {
+        get { return _leaderSectionX; }
+        set { if (_leaderSectionX != value) { _leaderSectionX = value; ChangeMapSection(); } }
+    }
+    int _leaderSectionY;
+    /// <summary>
+    /// 主角所处的切片Y值
+    /// </summary>
+    public int leaderSectionY
+    {
+        get { return _leaderSectionY; }
+        set { if (_leaderSectionY != value) { _leaderSectionY = value; ChangeMapSection(); } }
+    }
+
+    //Image mapSection;
+    int startSectionX, startSectionY, endSectionX, endSectionY;
+    /// <summary>
+    /// 初始化地图地表层
+    /// </summary>
+    //private void InitMapSurface(XElement args)
+    //{
+    //    MapSurface = new Canvas()
+    //    {
+    //        Width = Convert.ToDouble(args.Attribute("Width").Value),
+    //        Height = Convert.ToDouble(args.Attribute("Height").Value),
+    //    };
+    //    Add(MapSurface);
+    //    mapSectionWidth = Convert.ToInt32(args.Attribute("SectionWidth").Value);
+    //    mapSectionHeight = Convert.ToInt32(args.Attribute("SectionHeight").Value);
+    //    mapSectionXNum = (int)(MapSurface.Width / mapSectionWidth);
+    //    mapSectionYNum = (int)(MapSurface.Height / mapSectionHeight);
+    //}
+    private void ChangeMapSection()
+    {
+        //MapSurface.Children.Clear();
+        if (leaderSectionX == 0)
+        {
+            startSectionX = 0; endSectionX = 2;
+        }
+        else if (leaderSectionX == mapSectionXNum - 1)
+        {
+            startSectionX = leaderSectionX - 2; endSectionX = leaderSectionX;
+        }
+        else
+        {
+            startSectionX = leaderSectionX - 1; endSectionX = leaderSectionX + 1;
+        }
+        if (leaderSectionY == 0)
+        {
+            startSectionY = 0; endSectionY = 2;
+        }
+        else if (leaderSectionY == mapSectionYNum - 1)
+        {
+            startSectionY = leaderSectionY - 2; endSectionY = leaderSectionY;
+        }
+        else
+        {
+            startSectionY = leaderSectionY - 1; endSectionY = leaderSectionY + 1;
+        }
+        for (int x = startSectionX; x <= endSectionX; x++)
+        {
+            for (int y = startSectionY; y <= endSectionY; y++)
+            {
+                //mapSection = new Image()
+                //{
+                //    Source = Super.getImage(string.Format("Map/{0}/Surface/{1}_{2}.jpg", mapCode.ToString(), x, y)),
+                //    Width = mapSectionWidth,
+                //    Height = mapSectionHeight,
+                //    Stretch = Stretch.Fill,
+                //};
+                //MapSurface.Children.Add(mapSection);
+                //Canvas.SetLeft(mapSection, x * mapSectionWidth);
+                //Canvas.SetTop(mapSection, y * mapSectionHeight);
+            }
+        }
+    }
+
+    //public void checkCache(int x, int y)
+    //{
+
+    //    //      //////////////////
+    //    //      1-4,1-3,1-2,1-1
+    //    //      2-4,2-3,2-2,2-1 
+    //    //      3-4,3-3,3-2,3-1
+    //    //      4-4,4-3,4-2,4-1 
+    //    //      ////////////////// 2,2
+    //    cacheNewTemp.Clear();
+    //    cacheNew.Clear();
+    //    int leftx = x;
+    //    int lefty = y + 1;
+    //    int leftUpx = x - 1;
+    //    int leftUpy = y + 1;
+    //    int leftButtomx = x + 1;
+    //    int leftButtomy = y + 1;
+    //    int topx = x - 1;
+    //    int topy = y;
+    //    int buttomx = x + 1;
+    //    int buttomy = y;
+    //    int rightx = x;
+    //    int righty = y - 1;
+    //    int rightUpx = x - 1;
+    //    int rightUpy = y - 1;
+    //    int rightButtomx = x + 1;
+    //    int rightButtomy = y - 1;
+    //    if (cacheInit)
+    //    {
+
+    //        if (leftx >= 0 && lefty >= 0 && leftx < m_ZoneCount && lefty < m_ZoneCount)
+    //        {
+    //            getCahce(leftx, lefty);
+    //        }
+    //        if (leftUpx >= 0 && leftUpy >= 0 && leftUpx < m_ZoneCount && leftUpy < m_ZoneCount)
+    //        {
+    //            getCahce(leftUpx, leftUpy);
+    //        }
+    //        if (leftButtomx >= 0 && leftButtomy >= 0 && leftButtomx < m_ZoneCount && leftButtomy < m_ZoneCount)
+    //        {
+    //            getCahce(leftButtomx, leftButtomy);
+    //        }
+    //        if (topx >= 0 && topy >= 0 && topx < m_ZoneCount && topy < m_ZoneCount)
+    //        {
+    //            getCahce(topx, topy);
+    //        }
+    //        if (buttomx >= 0 && buttomy >= 0 && buttomx < m_ZoneCount && buttomy < m_ZoneCount)
+    //        {
+    //            getCahce(buttomx, buttomy);
+    //        }
+    //        if (rightx >= 0 && righty >= 0 && rightx < m_ZoneCount && righty < m_ZoneCount)
+    //        {
+    //            getCahce(rightx, righty);
+    //        }
+    //        if (rightUpx >= 0 && rightUpy >= 0 && rightUpx < m_ZoneCount && rightUpy < m_ZoneCount)
+    //        {
+    //            getCahce(rightUpx, rightUpy);
+    //        }
+    //        if (rightButtomx >= 0 && rightButtomy >= 0 && rightButtomx < m_ZoneCount && rightButtomy < m_ZoneCount)
+    //        {
+    //            getCahce(rightButtomx, rightButtomy);
+    //        }
+    //        string key = x + "," + y;
+    //        cacheNewTemp.Add(key, key);
+
+    //        getRemoveCahce();
+
+    //    }
+    //    else
+    //    {
+    //        if (leftx >= 0 && lefty >= 0 && leftx < m_ZoneCount && lefty < m_ZoneCount)
+    //        {
+    //            addCache(leftx, lefty);
+    //        }
+    //        if (leftUpx >= 0 && leftUpy >= 0 && leftUpx < m_ZoneCount && leftUpy < m_ZoneCount)
+    //        {
+    //            addCache(leftUpx, leftUpy);
+    //        }
+    //        if (leftButtomx >= 0 && leftButtomy >= 0 && leftButtomx < m_ZoneCount && leftButtomy < m_ZoneCount)
+    //        {
+    //            addCache(leftButtomx, leftButtomy);
+    //        }
+    //        if (topx >= 0 && topy >= 0 && topx < m_ZoneCount && topy < m_ZoneCount)
+    //        {
+    //            addCache(topx, topy);
+    //        }
+    //        if (buttomx >= 0 && buttomy >= 0 && buttomx < m_ZoneCount && buttomy < m_ZoneCount)
+    //        {
+    //            addCache(buttomx, buttomy);
+    //        }
+    //        if (rightx >= 0 && righty >= 0 && rightx < m_ZoneCount && righty < m_ZoneCount)
+    //        {
+    //            addCache(rightx, righty);
+    //        }
+    //        if (rightUpx >= 0 && rightUpy >= 0 && rightUpx < m_ZoneCount && rightUpy < m_ZoneCount)
+    //        {
+    //            addCache(rightUpx, rightUpy);
+    //        }
+    //        if (rightButtomx >= 0 && rightButtomy >= 0 && rightButtomx < m_ZoneCount && rightButtomy < m_ZoneCount)
+    //        {
+    //            addCache(rightButtomx, rightButtomy);
+    //        }
+    //        string key = x + "," + y;
+    //        cache.Add(key, key);
+    //        cacheInit = true;
+    //    }
+
+    //    getLoadNewCahce();
+    //}
+
+    //public void getRemoveCahce()
+    //{
+    //    Hashtable rtmp = new Hashtable();
+    //    IDictionaryEnumerator enumerator = cache.GetEnumerator();
+    //    while (enumerator.MoveNext())
+    //    {
+    //        Debug.Log("cache key = " + enumerator.Key.ToString());
+    //        if (!cacheNewTemp.Contains(enumerator.Key.ToString()))
+    //        {
+    //            rtmp.Add(enumerator.Key.ToString(), enumerator.Key.ToString());
+    //        }
+    //    }
+    //    IDictionaryEnumerator enumerator2 = rtmp.GetEnumerator();
+    //    while (enumerator2.MoveNext())
+    //    {
+    //        Debug.Log("del cache key = " + enumerator2.Key.ToString());
+    //        if (cache.Contains(enumerator2.Key.ToString()))
+    //        {
+    //            cache.Remove(enumerator2.Key.ToString());
+    //            string[] s = enumerator2.Key.ToString().Split(',');
+    //            StartCoroutine(UnloadZone(int.Parse(s[0]), int.Parse(s[1])));
+    //        }
+    //    }
+    //    rtmp.Clear();
+    //    rtmp = null;
+    //}
+    //public void getLoadNewCahce()
+    //{
+    //    IDictionaryEnumerator enumerator = cacheNew.GetEnumerator();
+    //    while (enumerator.MoveNext())
+    //    {
+    //        string[] s = enumerator.Key.ToString().Split(',');
+    //        StartCoroutine(LoadZone(int.Parse(s[0]), int.Parse(s[1])));
+    //    }
+    //}
+    //public void getCahce(int x, int y)
+    //{
+    //    string key = x + "," + y;
+    //    cacheNewTemp.Add(key, key);
+    //    if (!cache.Contains(key))
+    //    {
+    //        cache.Add(key, key);
+    //        cacheNew.Add(key, key);
+    //    }
+    //}
+    //public void addCache(int x, int y)
+    //{
+    //    string key = x + "," + y;
+    //    cache.Add(key, key);
+    //    StartCoroutine(LoadZone(x, y));
+    //}
+    //public void removeCache(int x, int y)
+    //{
+    //    string key = x + "," + y;
+    //    StartCoroutine(UnloadZone(x, y));
+    //}
+}
+
